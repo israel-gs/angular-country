d4a862fc6c9251adcb10dc9bbe282f1c881e9aae
israel-gs <israelgs83@gmail.com>
2024-02-09 00:45:12 -0500

    Actualización del componente de búsqueda***

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,12 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { Subject, debounceTime } from 'rxjs'
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
   @Input() placeholder: string = 'Search'
   @Output() onSearch: EventEmitter<string> = new EventEmitter()
+  private debouncer: Subject<string> = new Subject()
+
+  ngOnInit (): void {
+    this.debouncer.pipe(debounceTime(300)).subscribe(value => {
+      console.log(value)
+      this.handleSearch(value)
+    })
+  }
+
+  onKeyUp (value: string): void {
+    this.debouncer.next(value)
+  }
 
   handleSearch (value: string): void {
     this.onSearch.emit(value)
