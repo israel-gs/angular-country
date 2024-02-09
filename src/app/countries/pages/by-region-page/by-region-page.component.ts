5c661904295d9353671a7ffa3c4072b8116738a4
israel-gs <israelgs83@gmail.com>
2024-02-09 01:05:06 -0500

    Agregado de botones de búsqueda por región en la página de búsqueda por región

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core'
 import { Country } from '../../interfaces/country'
 import { CountriesService } from '../../services/countries.service'
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania'
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,6 +11,14 @@ import { CountriesService } from '../../services/countries.service'
 })
 export class ByRegionPageComponent {
   private _countries: Country[] = []
+  private _regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania'
+  ]
+  public selectedRegion?: Region
 
   constructor (private countriesService: CountriesService) {}
 
@@ -16,7 +26,12 @@ export class ByRegionPageComponent {
     return this._countries
   }
 
-  handleSearch (value: string) {
+  get regions () {
+    return this._regions
+  }
+
+  handleSearch (value: Region) {
+    this.selectedRegion = value
     this.countriesService.getByRegion(value).subscribe(countries => {
       this._countries = countries
     })
