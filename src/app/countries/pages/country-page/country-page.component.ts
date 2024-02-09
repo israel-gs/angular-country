5e26ea13d5c9a9e93ff60f65886823f436fecf6a
israel-gs <israelgs83@gmail.com>
2024-02-08 23:15:47 -0500

    Añadir plantilla de carga y detalles del país en la página de país.

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { CountriesService } from '../../services/countries.service'
+import { Country } from '../../interfaces/country'
+import { switchMap } from 'rxjs'
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  private _country: Country | null = null
 
+  constructor (
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ) {}
+
+  get country (): Country | null {
+    return this._country
+  }
+
+  get translationsCode () {
+    if (!this._country) return []
+    return Object.keys(this._country.translations)
+  }
+
+  ngOnInit (): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.countriesService.getByAlphaCode(id)))
+      .subscribe(country => {
+        if (!country) {
+          this.router.navigate([''])
+          return
+        }
+        this._country = country
+      })
+  }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Country } from '../interfaces/country'
-import { catchError, of } from 'rxjs'
+import { Observable, catchError, map, of } from 'rxjs'
 
 const BASE_URL = 'https://restcountries.com/v3.1'
 
@@ -10,6 +10,15 @@ const BASE_URL = 'https://restcountries.com/v3.1'
 })
 export class CountriesService {
   constructor (private httpClient: HttpClient) {}
+
+  getByAlphaCode (alphaCode: string): Observable<Country | null> {
+    return this.httpClient
+      .get<Country[]>(`${BASE_URL}/alpha/${alphaCode}`)
+      .pipe(
+        map(countries => countries[0] ?? null),
+        catchError(_ => of(null))
+      )
+  }
 
   getByCapital (capital: string) {
     return this.httpClient
